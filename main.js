28277d7d7bc7ae6da7c7fac4825644c92354e296
michaellidonni <526mlidonni@frhsd.com>
2025-11-30 18:34:19 -0500

    List Rendering

--- main.js
+++ main.js
@@ -6,7 +6,13 @@ const app = Vue.createApp({
             image: './assets/images/socks_green.jpg',
             url: 'https://marlboro.frhsd.com/',
             inventory: 100,
-            onSale: false
+            onSale: false,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue' }
+            ],
+            sizes: ['S', 'M', 'L', 'XS']
         }
     }
 })
