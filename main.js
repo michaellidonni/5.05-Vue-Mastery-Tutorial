7cec10d6ee895fa862b19c04c0f472fa15ade8ba
michaellidonni <526mlidonni@frhsd.com>
2025-11-30 18:21:07 -0500

    Attribute Binding

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             description: 'A warm and fuzzy pair of socks',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://marlboro.frhsd.com/',
         }
     }
 })
