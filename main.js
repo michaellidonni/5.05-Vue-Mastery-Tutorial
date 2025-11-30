b8fb055f0dfdc335eacae6635ac719a209101c84
michaellidonni <526mlidonni@frhsd.com>
2025-11-30 18:44:03 -0500

    Event Handling

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart: 0,
             product: 'Socks',
             description: 'A warm and fuzzy pair of socks',
             image: './assets/images/socks_green.jpg',
@@ -9,10 +10,21 @@ const app = Vue.createApp({
             onSale: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green' },
-                { id: 2235, color: 'blue' }
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
             ],
             sizes: ['S', 'M', 'L', 'XS']
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
+        },
+        removeFromCart() {
+            this.cart -= 1
         }
     }
 })
